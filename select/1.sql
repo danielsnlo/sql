9d64db5032e02461620a2450b3b6fae2b7c29e75
danielsnlo <danielsnlo@example.com>
2024-09-06 08:54:51 +0300

    basic select with where and order by clauses

---
+++ select/1.sql
@@ -0,0 +1,13 @@
+-- use sql_store;
+-- SELECT 2*5, "result"
+
+-- use sql_invoicing;
+-- select * from clients
+-- WHERE client_id = 1;
+-- WHERE state = 'NY';
+-- ORDER BY state;
+
+use sql_invoicing;
+select * from invoices
+where payment_total > 0
+order by client_id;

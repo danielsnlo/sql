212d33d40f3599508293a23b9704a39124e12c88
danielsnlo <danielsnlo@example.com>
2024-09-12 10:55:47 +0300

    joins with a task added at the bottom

--- 2select-join/outer-join.sql
+++ 2select-join/outer-join.sql
@@ -21,4 +21,27 @@ using (customer_id);
 SELECT * FROM
 customers c
 RIGHT JOIN orders o
-using (customer_id)
+using (customer_id);
+
+
+
+-- UZDEVUMS
+-- atlasīt produktus un pasūtījuma vienības
+-- tā, lai tiktu atlasīti visi produkti
+-- arī tādi, kuri nav pasūtīti
+
+use sql_store;
+SELECT 
+   -- *
+    product_id as "Product ID",
+    name as "Name",
+    quantity_in_stock as "Quantity in Stock",
+    p.unit_price as "Product Unit Price",
+    order_id as "Order ID",
+    quantity as "Quantity",
+    oi.unit_price as "Unit Price"
+    
+FROM
+    products p
+        LEFT JOIN
+    order_items oi USING (product_id)
